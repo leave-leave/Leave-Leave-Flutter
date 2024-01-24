7f3e40418aaf34b55814c33d624b486378edabb9
yerin seo <syr1130@dsm.hs.kr>
2024-01-24 22:10:55 +0900

    ADD :: code

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -10,11 +10,54 @@ class MainScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 24,),
-          Center(
+          SizedBox(height: 24),
+          Align(
             child: Image.asset('assets/images/tteonatteonabar.png'),
-          )
-
+          ),
+          Align(
+            child: Image.asset('assets/images/main.png'),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    '요즘 인기 여행지',
+                    style: TextStyle(
+                      color: Color(0xFF2D2D2D),
+                      fontSize: 14,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w500,
+                      height: 0
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('더보기',
+                  style: TextStyle(
+                    color: Color(0xFF474747),
+                    fontSize: 14,
+                    fontFamily: 'Noto Sans KR',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              '설문 결과를 통해 여행지를 추천해 드려요',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Noto sans KR',
+              ),
+            ),
+          ),
         ],
       ),
     );
