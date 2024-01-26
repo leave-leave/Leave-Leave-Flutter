481e5912693406112535856e6212d1ccb9a5b0ea
yerin seo <syr1130@dsm.hs.kr>
2024-01-26 20:11:55 +0900

    ADD :: file 추가

---
+++ lib/screen/feed.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class post extends StatelessWidget {
+  const post({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffECF3FF),
+    );
+  }
+}
---
+++ lib/screen/post_upload_complete.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:tteonatteona/screen/feed.dart';
+
+class post_upload_complete extends StatelessWidget {
+
+  const post_upload_complete({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => post()),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Color(0xffECF3FF),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset('assets/images/post_uploade.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
