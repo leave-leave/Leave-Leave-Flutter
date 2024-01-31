56f26010543f6dff7ac9f5cac359bcd704a271b7
yerin seo <syr1130@dsm.hs.kr>
2024-01-31 22:42:55 +0900

    UPDATE :: code

--- lib/screen/mypage.dart
+++ lib/screen/mypage.dart
@@ -12,6 +12,58 @@ class _mypageState extends State<mypage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffECF3FF),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 24),
+          Row(
+            children: [
+              SizedBox(width: 10),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back, size: 20),
+              ),
+              Text(
+                '마이 페이지',
+                style: TextStyle(
+                  color: Color(0xFF111111),
+                  fontSize: 16,
+                  fontFamily: 'Noto Sans KR',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Container(
+            padding: EdgeInsets.only(left: 38),
+            width: 411,
+            height: 115,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Row(
+              children: [
+                Image.asset('assets/images/profile.png'),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '서예린',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
