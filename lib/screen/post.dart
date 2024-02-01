02596f50f8869c0728c2b81d9277991137709206
yerin seo <syr1130@dsm.hs.kr>
2024-02-01 20:12:53 +0900

    ADD :: code

--- lib/screen/post.dart
+++ lib/screen/post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tteonatteona/screen/post_add.dart';
 
 
 class post extends StatefulWidget {
@@ -16,27 +17,38 @@ class _postState extends State<post> {
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          SizedBox(height: 24),
-      Row(
-        children: [
-          SizedBox(width: 10),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.arrow_back, size: 20),
-          ),
-          Text(
-            '게시물',
-            style: TextStyle(
-              color: Color(0xFF111111),
-              fontSize: 16,
-              fontFamily: 'Noto Sans KR',
-              fontWeight: FontWeight.w500,
+            SizedBox(height: 24),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                IconButton(
+                  onPressed: () {
+                  },
+                  icon: Icon(Icons.arrow_back, size: 20),
+                ),
+                Text(
+                  '게시물',
+                  style: TextStyle(
+                    color: Color(0xFF111111),
+                    fontSize: 16,
+                    fontFamily: 'Noto Sans KR',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                IconButton(
+                  padding: EdgeInsets.only(left: 271),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => postadd()),
+                    );
+                  },
+                  icon: Icon(Icons.add, size: 24),
+                ),
+              ],
             ),
-          ),
-        ],
+          ]
       ),
-      ]
-    ),
     );
   }
 }
