a2d7cbba403d9ece3566caf9bddc3e57a95b92f1
yerin seo <syr1130@dsm.hs.kr>
2024-02-08 23:39:53 +0900

    ADD :: code

--- lib/suggestion/suggestion.dart
+++ lib/suggestion/suggestion.dart
@@ -8,10 +8,35 @@ class suggestion extends StatefulWidget {
 }
 
 class _suggestionState extends State<suggestion> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffECF3FF),
+      body: Column(
+        children: [
+          SizedBox(height: 24),
+          Align(
+            child: Image.asset('assets/images/tteonatteonabar2.png'),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '예린님, 이 여행지는 어때요?',
+                style: TextStyle(
+                  color: Color(0xff1E3999),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
