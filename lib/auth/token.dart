ad7117ac319f8a161544b6507acdedf9c74f48e8
yerin seo <syr1130@dsm.hs.kr>
2024-02-09 22:41:42 +0900

    ADD :: File

---
+++ lib/auth/token.dart
@@ -0,0 +1,18 @@
+class RefreshToken {
+  String? accessToken;
+  String? refreshToken;
+
+  RefreshToken({this.accessToken, this.refreshToken});
+
+  RefreshToken.fromJson(Map<String, dynamic> json) {
+    accessToken = json['access_token'];
+    refreshToken = json['refresh_tokke'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['access_token'] = this.accessToken;
+    data['refresh_tokke'] = this.refreshToken;
+    return data;
+  }
+}
