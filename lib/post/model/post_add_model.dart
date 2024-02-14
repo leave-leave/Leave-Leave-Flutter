b36cdc7f00c85a055752cb9c95decdd7861f0ad1
seoyerin <us3544974@gmail.com>
2024-02-15 00:22:54 +0900

    ADD :: model

---
+++ lib/post/model/post_add_model.dart
@@ -0,0 +1,18 @@
+class postmodel {
+  String? title;
+  String? content;
+
+  postmodel({this.title, this.content});
+
+  postmodel.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    content = json['content'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['content'] = this.content;
+    return data;
+  }
+}
