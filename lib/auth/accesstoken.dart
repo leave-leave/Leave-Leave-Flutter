4ff9d6dd40fa4a38267df009250f7340b249b9e0
seoyerin <us3544974@gmail.com>
2024-02-18 14:24:54 +0900

    ADD :: model

---
+++ lib/auth/accesstoken.dart
@@ -0,0 +1,36 @@
+class accesstokenresponse {
+  Token? token;
+
+  accesstokenresponse({this.token});
+
+  accesstokenresponse.fromJson(Map<String, dynamic> json) {
+    token = json['token'] != null ? new Token.fromJson(json['token']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.token != null) {
+      data['token'] = this.token!.toJson();
+    }
+    return data;
+  }
+}
+
+class Token {
+  String? accessToken;
+  String? refreshToken;
+
+  Token({this.accessToken, this.refreshToken});
+
+  Token.fromJson(Map<String, dynamic> json) {
+    accessToken = json['access_token'];
+    refreshToken = json['refresh_token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['access_token'] = this.accessToken;
+    data['refresh_token'] = this.refreshToken;
+    return data;
+  }
+}
