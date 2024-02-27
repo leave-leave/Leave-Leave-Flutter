c2594a453b5ab8cae521b48dbbae367ebb7cc2be
seoyerin <us3544974@gmail.com>
2024-02-27 23:48:56 +0900

    ADD :: package

--- lib/question/widget/question_screen.dart
+++ lib/question/widget/question_screen.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:tteonatteona/secret.dart';
 import 'dart:convert';
 import 'package:tteonatteona/question/model/question.dart';
+import 'package:tteonatteona/suggestion/suggestion_explain.dart';
 
 class question extends StatelessWidget {
   const question({Key? key}) : super(key: key);
@@ -110,7 +111,9 @@ class question extends StatelessWidget {
                   height: 40,
                   child: ElevatedButton(
                     onPressed: () {
-
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => suggestion_explain()),
+                      );
                     },
                     child: Text('완료', style: TextStyle(
                       color: Colors.white
@@ -130,79 +133,94 @@ class question extends StatelessWidget {
 }
 
 
-class questionList extends StatelessWidget {
+class questionList extends StatefulWidget {
   const questionList({Key? key}) : super(key: key);
 
   @override
+  _QuestionListState createState() => _QuestionListState();
+}
+
+class _QuestionListState extends State<questionList> {
+  List<bool> buttonClickedList = List.generate(6, (index) => false);
+  List<bool> buttonClickedList2 = List.generate(6, (index) => false);
+
+  List<String> texts = [    '1. 바다를 좋아하나요?',    '2. 방구석 여행을 즐기나요?',    '3. 탐험을 좋아하나요?',    '4. 관광을 즐기나요?',    '5. 답사를 즐기나요?',    '6. 먹는것을 즐기나요?',  ];
+
+  @override
   Widget build(BuildContext context) {
-
-    List<String> texts = [
-      '1. 바다를 좋아하나요?',
-      '2. 방구석 여행을 즐기나요?',
-      '3. 탐험을 좋아하나요?',
-      '4. 관광을 즐기나요?',
-      '5. 답사를 즐기나요?',
-      '6. 먹는것을 즐기나요?',
-    ];
-
     return Container(
       child: ListView.builder(
-      shrinkWrap: true,
-      padding: EdgeInsets.only(left: 20, right: 20),
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        final backgroundColor = index.isEven ? Color(0xff6386FF) : Colors.white;
-        final iconColor = index.isEven ? Colors.white : Colors.black;
-        final text = texts[index];
-        return Column(
-          children: [
-            Container(
-              height: 70,
-              margin: EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                border: Border.all(
-                  color: Color(0xff6386FF),
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 20, right: 20),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          final backgroundColor =
+          index.isEven ? Color(0xff6386FF) : Color(0xffDEE8FC);
+          final iconColor = index.isEven ? Colors.white : Colors.black;
+          final text = texts[index];
+          final buttonBackgroundColor =
+          buttonClickedList[index] ? Colors.red : Colors.green;
+
+          return Column(
+            children: [
+              Container(
+                height: 70,
+                margin: EdgeInsets.symmetric(vertical: 12),
+                decoration: BoxDecoration(
+                  color: backgroundColor,
+                  border: Border.all(
+                    color: Color(0xff6386FF),
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(left: 24),
-                child: Row(
-                  children: [
-                    Text(
-                      text,
-                      style: TextStyle(
-                        color: iconColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: Row(
+                    children: [
+                      Text(
+                        text,
+                        style: TextStyle(
+                          color: iconColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Padding(padding: EdgeInsets.only(right: 20),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.panorama_fish_eye, size: 30),
-                            color: backgroundColor == Colors.white ? Color(0xff505050) : Colors.white,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.close, size: 33),
-                            color: backgroundColor == Colors.white ? Color(0xff505050) : Colors.white,
-                          ),
-                        ],
+                      Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  buttonClickedList[index] =
+                                  !buttonClickedList[index];
+                                });
+                              },
+                              icon: Icon(Icons.panorama_fish_eye, size: 30),
+                              color: buttonClickedList[index] ? Colors.white : Colors.black,
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  buttonClickedList2[index] =
+                                  !buttonClickedList2[index];
+                                });
+                              },
+                              icon: Icon(Icons.close, size: 33),
+                              color: buttonClickedList2[index] ? Colors.white : Colors.black,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        );
-      },
+            ],
+          );
+        },
       ),
     );
   }
