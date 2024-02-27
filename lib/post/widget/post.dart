b22d44f86a2d0b0aa8906c8ab5c6316543d5a926
seoyerin <us3544974@gmail.com>
2024-02-27 23:49:25 +0900

    REFACTOR :: code

--- lib/post/widget/post.dart
+++ lib/post/widget/post.dart
@@ -3,9 +3,8 @@ import 'package:tteonatteona/post/widget/post_add.dart';
 import 'package:tteonatteona/post/widget/post_details.dart';
 import 'package:dio/dio.dart';
 import 'package:tteonatteona/secret.dart';
-import 'dart:convert';
 import '../model/post_check.dart';
-import 'package:tteonatteona/post/model/post_check.dart';
+
 
 class Post extends StatefulWidget {
   const Post({Key? key}) : super(key: key);
@@ -38,11 +37,18 @@ class _PostState extends State<Post> {
     }
   }
 
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
   Future<List<PostCheck>> fetchPosts() async {
     Dio dio = Dio();
     List<PostCheck> posts = [];
 
     try {
+      List<PostCheck> posts = await fetchPosts();
       final resp = await dio.get(
         "$baseUrl/feeds",
         options: Options(
@@ -83,7 +89,6 @@ class _PostState extends State<Post> {
         ),
       );
       print(resp.statusCode);
-
     } catch (e) {
       print('에러');
       throw Exception(e);
@@ -104,134 +109,250 @@ class _PostState extends State<Post> {
         ),
       );
       print(resp.statusCode);
-
     } catch (e) {
       print('에러');
       throw Exception(e);
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffECF3FF),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(width: 25),
-              Text(
-                '게시물',
-                style: TextStyle(
-                  color: Color(0xFF111111),
-                  fontSize: 16,
-                  fontFamily: 'Noto Sans KR',
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(width: 280),
-              SizedBox(
-                child: IconButton(
-                  onPressed: () {
+      body: FutureBuilder<List<PostCheck>>(
+        future: fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('에러 발생'),
+            );
+          } else if (snapshot.hasData) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: snapshot.data!.map((post) {
+                return InkWell(
+                  onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => PostAdd()),
+                      MaterialPageRoute(builder: (context) => PostDetails()),
                     );
                   },
-                  icon: Icon(Icons.add, size: 24),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PostDetails()),
-              );
-            },
-            child: Container(
-              height: 375,
-              margin: EdgeInsets.only(left: 24, right: 24),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      SizedBox(width: 16),
-                      Icon(
-                        Icons.account_circle,
-                        color: Color(0xff2B8AFB),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        '서예린',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
+                  child: Container(
+                    height: 375,
+                    margin: EdgeInsets.only(left: 24, right: 24),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            SizedBox(width: 16),
+                            Icon(
+                              Icons.account_circle,
+                              color: Color(0xff2B8AFB),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              post.userName,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(width: 220),
+                            Icon(Icons.delete)
+                          ],
                         ),
-                      ),
-                      SizedBox(width: 220),
-                      Icon(Icons.delete)
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Padding(
-                    padding: EdgeInsets.only(left: 34),
-                    child: Image.asset('assets/images/post.png'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 30),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isLiked = !isLiked;
-                            if (isLiked) {
-                              likeCount++;
-                            } else {
-                              likeCount--;
-                            }
-                          });
-                        },
-                        icon: isLiked
-                            ? Icon(Icons.favorite, size: 24, color: Colors.blue)
-                            : Icon(Icons.favorite_border,
-                            size: 24, color: Colors.blue),
-                      ),
-                      Text(likeCount.toString()),
-                      IconButton(
-                        onPressed: () {
-                          // Add functionality for comments button
-                        },
-                        icon: Icon(Icons.mode_comment_outlined,
-                            size: 24, color: Colors.blue),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 36),
-                    child: Text(
-                      'ㅇㄴㄹㄴㅁㅇㄹㄴㅇㄹㅇㄴ',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
-                      ),
+                        SizedBox(height: 16),
+                        Padding(
+                          padding: EdgeInsets.only(left: 34),
+                          child: Image.network(post.imageUrl),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(width: 30),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isLiked = !isLiked;
+                                  if (isLiked) {
+                                    likeCount++;
+                                  } else {
+                                    likeCount--;
+                                  }
+                                });
+                              },
+                              icon: isLiked
+                                  ? Icon(
+                                  Icons.favorite, size: 24, color: Colors.blue)
+                                  : Icon(Icons.favorite_border,
+                                  size: 24, color: Colors.blue),
+                            ),
+                            Text(likeCount.toString()),
+                            IconButton(
+                              onPressed: () {
+                                // Add functionality for comments button
+                              },
+                              icon: Icon(Icons.mode_comment_outlined,
+                                  size: 24, color: Colors.blue),
+                            )
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 36),
+                          child: Text(
+                            post.content, // 게시물 내용
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ],
+                );
+              }).toList(),
+            );
+          } else {
+            return Center(
+              child: Text('데이터 없음'), // 데이터 없음 메시지 표시
+            );
+          }
+        },
       ),
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xffECF3FF),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           SizedBox(height: 30),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               SizedBox(width: 25),
+//               Text(
+//                 '게시물',
+//                 style: TextStyle(
+//                   color: Color(0xFF111111),
+//                   fontSize: 16,
+//                   fontFamily: 'Noto Sans KR',
+//                   fontWeight: FontWeight.w500,
+//                 ),
+//               ),
+//               SizedBox(width: 280),
+//               SizedBox(
+//                 child: IconButton(
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => PostAdd()),
+//                     );
+//                   },
+//                   icon: Icon(Icons.add, size: 24),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: 10),
+//           InkWell(
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => PostDetails()),
+//               );
+//             },
+//             child: Container(
+//               height: 375,
+//               margin: EdgeInsets.only(left: 24, right: 24),
+//               color: Colors.white,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   SizedBox(height: 16),
+//                   Row(
+//                     children: [
+//                       SizedBox(width: 16),
+//                       Icon(
+//                         Icons.account_circle,
+//                         color: Color(0xff2B8AFB),
+//                       ),
+//                       SizedBox(width: 8),
+//                       Text(
+//                         '서예린',
+//                         style: TextStyle(
+//                           fontSize: 14,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                       SizedBox(width: 220),
+//                       Icon(Icons.delete)
+//                     ],
+//                   ),
+//                   SizedBox(height: 16),
+//                   Padding(
+//                     padding: EdgeInsets.only(left: 34),
+//                     child: Image.asset('assets/images/post.png'),
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     children: [
+//                       SizedBox(width: 30),
+//                       IconButton(
+//                         onPressed: () {
+//                           setState(() {
+//                             isLiked = !isLiked;
+//                             if (isLiked) {
+//                               likeCount++;
+//                             } else {
+//                               likeCount--;
+//                             }
+//                           });
+//                         },
+//                         icon: isLiked
+//                             ? Icon(Icons.favorite, size: 24, color: Colors.blue)
+//                             : Icon(Icons.favorite_border,
+//                             size: 24, color: Colors.blue),
+//                       ),
+//                       Text(likeCount.toString()),
+//                       IconButton(
+//                         onPressed: () {
+//                           // Add functionality for comments button
+//                         },
+//                         icon: Icon(Icons.mode_comment_outlined,
+//                             size: 24, color: Colors.blue),
+//                       )
+//                     ],
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 36),
+//                     child: Text(
+//                       '나는 영국에 왔어',
+//                       style: TextStyle(
+//                         fontSize: 14,
+//                         color: Colors.black,
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
