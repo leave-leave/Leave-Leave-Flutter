255c5aae240fab3b15be1fec3bd2874a0fb71555
seoyerin <us3544974@gmail.com>
2024-03-02 12:03:08 +0900

    SET

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/eugnwo/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/eugnwo/Documents/flutter_projects/leave-leave-flutter"
+export "FLUTTER_ROOT=/Users/user/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user/Desktop/leave-leave-flutter-main"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
